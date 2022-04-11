7a52f5a430139e9b4d82865194bc1d44b31c211f
Renan S Moreira <renan.siravegna@gmail.com>
2022-04-10 21:35:06 -0400

    Creating a object to deal with money and its rounding rules

---
+++ core/src/domain/remunerations/money.ts
@@ -0,0 +1,19 @@
+import { Currency } from './currency';
+
+export class Money {
+  private _amount: number;
+  private _currency: Currency;
+
+  constructor(amount: number, currency: Currency) {
+    this._amount = Math.round(amount * 100) / 100;
+    this._currency = currency;
+  }
+
+  get amount(): number {
+    return this._amount;
+  }
+
+  get currency(): number {
+    return this._currency;
+  }
+}
---
+++ core/test/domain/remunerations/money.test.ts
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import { Currency } from '../../../src/domain/remunerations/currency';
+import { Money } from '../../../src/domain/remunerations/money';
+
+describe('Money', () => {
+  it('should be created with value', () => {
+    const expectedAmount = 100;
+
+    const money = new Money(expectedAmount, Currency.REAL);
+
+    expect(money.amount).to.be.equal(expectedAmount);
+  });
+
+  it('should be created with currency', () => {
+    const expectedCurrency = Currency.REAL;
+
+    const money = new Money(100, expectedCurrency);
+
+    expect(money.currency).to.be.equal(expectedCurrency);
+  });
+
+  [
+    [1.699, 1.70],
+    [2.759, 2.76],
+    [10.659, 10.66],
+    [1.6666666666666665, 1.67]
+  ].forEach(([rawAmount, expectedRoundedAmount]) => {
+    it(`should round values: ${rawAmount}, ${expectedRoundedAmount}`, () => {
+      const money = new Money(rawAmount, Currency.REAL);
+
+      expect(money.amount).to.be.equal(expectedRoundedAmount);
+    });
+  });
+
+  [
+    [2.35],
+    [300.11],
+    [55.99]
+  ].forEach(([rawAmount]) => {
+    it(`should not round values with less than 3 decimal places: ${rawAmount}`, () => {
+      const money = new Money(rawAmount, Currency.REAL);
+
+      expect(money.amount).to.be.equal(rawAmount);
+    });
+  });
+});
