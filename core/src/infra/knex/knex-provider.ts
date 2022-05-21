880fa58956f61fc50c65a970dba2bfa63d53999c
Renan S Moreira <renan.siravegna@gmail.com>
2022-05-21 19:54:48 -0400

    Fix Knex config file

--- core/src/infra/knex/knex-provider.ts
+++ core/src/infra/knex/knex-provider.ts
@@ -1,13 +1,12 @@
 import knex, { Knex } from 'knex';
-import path from 'path';
-import asd from './knexfile';
+import knexConfig from './knexfile';
 
 export class KnexProvider {
   session?: Knex;
 
   async getSession(): Promise<Knex> {
     if (!this.session) {
-      this.session = knex(asd.development);
+      this.session = knex(knexConfig.development);
     }
 
     return this.session;
