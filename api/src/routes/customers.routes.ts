fd019aa0cbe86952c56b94b2219813c7884d1b20
Renan S Moreira <renan.siravegna@gmail.com>
2022-05-22 11:39:01 -0400

    Adding get/put/post for customers API

--- api/src/routes/customers.routes.ts
+++ api/src/routes/customers.routes.ts
@@ -3,6 +3,8 @@ import { Link } from '../serializers/json-api/link';
 import { Links } from '../serializers/json-api/links';
 import { CustomersKnex } from 'timetracker-core/src/infra/knex/customers-knex';
 import { KnexProvider } from 'timetracker-core/src/infra/knex/knex-provider';
+import { Customer } from 'timetracker-core/src/domain/customers/customer';
+import { Id } from 'timetracker-core/src/domain/id';
 
 const router = express.Router();
 const provider = new KnexProvider();
@@ -34,6 +36,67 @@ router.get('/customers', async (_req, res) => {
         .add(new Link('self', '/customers'))
         .serialize()
     });
+});
+
+router.get('/customers/:id', async (req, res) => {
+  const id = new Id(req.params.id);
+  const customer = await customers.get(id);
+
+  res.json({
+    meta: {
+      template: {
+        POST: [
+          { name: 'name', type: 'string', displayName: 'Name', editable: true }
+        ],
+        PUT: [
+          { name: 'id', type: 'string', displayName: 'Id' },
+          { name: 'name', type: 'string', displayName: 'Name', editable: true }
+        ],
+      }
+    },
+    type: 'customers',
+    id: id.toString(),
+    attributes: {
+      name: customer?.name
+    },
+    relationships: {},
+    links: Links.new()
+      .add(new Link('self', `/customers/${id}`))
+      .serialize()
+  });
+});
+
+router.post('/customers', async (req, res) => {
+  const customer = new Customer(req.body.name);
+  await customers.save(customer);
+
+  res.json({
+    type: 'customers',
+    id: customer.id,
+    relationships: {},
+    links: Links.new()
+      .add(new Link('self', `/customers/${customer.id}`))
+      .serialize()
+  });
+});
+
+router.put('/customers/:id', async (req, res) => {
+  const id = new Id(req.params.id);
+  const customer = await customers.get(id);
+
+  if (customer) {
+    customer.changeName(req.body.name);
+    await customers.save(customer);
+  }
+
+  res.json({
+    type: 'customers',
+    id: id,
+    relationships: {},
+    links: Links.new()
+      .add(new Link('self', `/customers/${id}`))
+      .serialize()
+  });
 });
 
 export default router;
