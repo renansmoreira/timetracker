ceacab20803474bd7ddd25ab7b158b9cd30b2587
Renan S Moreira <renan.siravegna@gmail.com>
2022-06-18 11:22:26 -0400

    Fix time display

--- app/src/components/time/Time.tsx
+++ app/src/components/time/Time.tsx
@@ -12,9 +12,9 @@ export default function Time({ value }: Props) {
 
   const valueInSeconds = parseInt((value || '').toString()) / 1000;
   const hours = valueInSeconds / 3600;
-  const minutes = valueInSeconds / 60;
-  const seconds = (60 * (minutes % 1))/100;
-  const displayValue = `${padLeft(hours - hours % 1)}:${padLeft(minutes - minutes % 1)}:${seconds.toString().substring(2, 4)}`;
+  const minutes = (60 * (hours % 1)) / 100;
+  const seconds = (60 * (minutes % 1)) / 100;
+  const displayValue = `${padLeft(hours - hours % 1)}:${minutes.toString().substring(2, 4)}:${seconds.toString().substring(2, 4)}`;
 
   return (
     <span>{displayValue}</span>
